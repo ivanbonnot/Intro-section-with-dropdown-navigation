91d1021cef7f1759b6df1fefca6094d8fa24d1c8
Ivan Bonnot <ivanbonnot@gmail.com>
2022-06-01 14:17:48 -0300

    mediaq v1

--- js/script.js
+++ js/script.js
@@ -3,12 +3,13 @@ const company =  document.querySelector(".company");
 const featuresMenu = document.querySelector(".features-menu");
 const companyMenu = document.querySelector(".company-menu");
 
-features.addEventListener('mouseenter', ()=> {featuresMenu.classList.add('active')})
-features.addEventListener('mouseleave', ()=> {featuresMenu.classList.remove('active')})
-featuresMenu.addEventListener('mouseenter', ()=> {featuresMenu.classList.add('active')})
-featuresMenu.addEventListener('mouseleave', ()=> {featuresMenu.classList.remove('active')})
 
-company.addEventListener('mouseenter', ()=> {companyMenu.classList.add('active')})
-company.addEventListener('mouseleave', ()=> {companyMenu.classList.remove('active')})
-companyMenu.addEventListener('mouseenter', ()=> {companyMenu.classList.add('active')})
-companyMenu.addEventListener('mouseleave', ()=> {companyMenu.classList.remove('active')})
+features.addEventListener('mouseenter', ()=> {featuresMenu.style.visibility = 'visible'})
+features.addEventListener('mouseleave', ()=> {featuresMenu.style.visibility = 'hidden'})
+featuresMenu.addEventListener('mouseenter', ()=> {featuresMenu.style.visibility = 'visible'})
+featuresMenu.addEventListener('mouseleave', ()=> {featuresMenu.style.visibility = 'hidden'})
+
+company.addEventListener('mouseenter', ()=> {companyMenu.style.visibility = 'visible'})
+company.addEventListener('mouseleave', ()=> {companyMenu.style.visibility = 'hidden'})
+companyMenu.addEventListener('mouseenter', ()=> {companyMenu.style.visibility = 'visible'})
+companyMenu.addEventListener('mouseleave', ()=> {companyMenu.style.visibility = 'hidden'})
