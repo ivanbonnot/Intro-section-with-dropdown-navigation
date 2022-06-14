175eb5c67af8e03e0fc2eb13bcf951a055fccff9
Ivan Bonnot <ivanbonnot@gmail.com>
2022-06-14 20:43:41 -0300

    nada

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-const features = document.querySelector(".features");
-const company =  document.querySelector(".company");
-const featuresMenu = document.querySelector(".features-submenu");
-const companyMenu = document.querySelector(".company-submenu");
-
-
 
 let mediaqueryList = window.matchMedia("(max-width: 375px)");
 
@@ -11,12 +5,3 @@ if(mediaqueryList.matches) {
     
   }
 
-features.addEventListener('mouseenter', ()=> {featuresMenu.style.visibility = 'visible'})
-features.addEventListener('mouseleave', ()=> {featuresMenu.style.visibility = 'hidden'})
-featuresMenu.addEventListener('mouseenter', ()=> {featuresMenu.style.visibility = 'visible'})
-featuresMenu.addEventListener('mouseleave', ()=> {featuresMenu.style.visibility = 'hidden'})
-
-company.addEventListener('mouseenter', ()=> {companyMenu.style.visibility = 'visible'})
-company.addEventListener('mouseleave', ()=> {companyMenu.style.visibility = 'hidden'})
-companyMenu.addEventListener('mouseenter', ()=> {companyMenu.style.visibility = 'visible'})
-companyMenu.addEventListener('mouseleave', ()=> {companyMenu.style.visibility = 'hidden'})
