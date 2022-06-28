3c17c5dbf08065f28467023cef786d21b0738fda
Ivan Bonnot <ivanbonnot@gmail.com>
2022-06-28 07:59:53 -0300

    test js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
+const features = document.querySelector(".features");
+const featuresMenu = document.querySelector(".features-menu");
+const endPart = document.querySelector('.end');
 
-let mediaqueryList = window.matchMedia("(max-width: 375px)");
+let mediaqueryList = window.matchMedia("(max-width: 740px)");
 
 if(mediaqueryList.matches) {
-    
+    features.addEventListener("click", ()=> {
+      featuresMenu.style.margin = '0px 0px 160px 0px';
+      endPart.style.margin = '300px 0px 0px 0px'
+
+    })
   }
