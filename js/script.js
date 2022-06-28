d375d11e78a60587f12cc630e381f382510753e4
Ivan Bonnot <ivanbonnot@gmail.com>
2022-06-27 21:33:32 -0300

    navbar mobile js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,64 @@
+// const features = document.querySelector(".features");
+// const featuresMenu = document.getElementById("feat-submenu");
 
-let mediaqueryList = window.matchMedia("(max-width: 375px)");
+// const endPart = document.querySelector('.end');
 
-if(mediaqueryList.matches) {
-    
-  }
-<<<<<<< HEAD
 
-=======
->>>>>>> 7efe3b68a3d8549cd8d076c8a4527d4ee57ab0e2
+// let mediaqueryList = window.matchMedia("(max-width: 740px)");
+
+// if (mediaqueryList.matches) {
+//   features.addEventListener("click", () => {
+//     featuresMenu.style.margin = '0px 0px 160px 0px';
+//     endPart.style.margin = '360px 0px 0px 0px';
+//   })
+// }
+
+const features = document.querySelector(".features");
+const featuresMenu = document.querySelector(".features-menu");
+const company = document.querySelector(".company");
+const companyMenu = document.querySelector(".company-menu");
+
+const navbar = document.querySelector(".navbar");
+const menu = document.querySelector(".menu");
+const close = document.querySelector(".close");
+
+const endPart = document.querySelector('.end');
+
+
+let mediaqueryList = window.matchMedia("(max-width: 740px)");
+
+if (mediaqueryList.matches) {
+
+  menu.addEventListener("mouseenter", ()=> {
+    navbar.style.display = 'flex';
+    menu.style.display = 'none';
+    close.style.display = 'block';
+  })
+  
+  
+  close.addEventListener("mouseenter",()=> {
+    navbar.style.display = 'none';
+  })
+  
+  
+  navbar.addEventListener("mouseenter", ()=> {
+    navbar.style.display = 'flex';
+  })
+
+  features.addEventListener("mouseenter", () => {
+    featuresMenu.style.margin = '0px 0px 160px 0px';
+    endPart.style.margin = '360px 0px 0px 0px';
+  })
+  features.addEventListener("mouseleave", () => {
+    featuresMenu.style.margin = '0px 0px 0px 0px';
+    endPart.style.margin = '0px 0px 0px 0px';
+  })
+  company.addEventListener("mouseenter", () => {
+    companyMenu.style.margin = '0px 0px 160px 0px';
+    endPart.style.margin = '360px 0px 0px 0px';
+  })
+  company.addEventListener("mouseleave", () => {
+    companyMenu.style.margin = '0px 0px 0px 0px';
+    endPart.style.margin = '0px 0px 0px 0px';
+  })
+}
